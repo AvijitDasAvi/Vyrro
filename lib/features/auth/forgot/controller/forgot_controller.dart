c8dcf3e1426fe8f4e47b4ac720b83e0bffb20348
AvijitDasAvi <avijitavi338895@gmail.com>
2026-03-01 06:12:41 +0600

    Complete forget password screen.

--- lib/features/auth/forgot/controller/forgot_controller.dart
+++ lib/features/auth/forgot/controller/forgot_controller.dart
@@ -1,3 +1,13 @@
 import 'package:get/get.dart';
 
-class ForgotController extends GetxController {}
+class ForgotController extends GetxController {
+  final RxBool isEmailSelected = true.obs;
+
+  void selectEmail() {
+    isEmailSelected.value = true;
+  }
+
+  void selectPhone() {
+    isEmailSelected.value = false;
+  }
+}
